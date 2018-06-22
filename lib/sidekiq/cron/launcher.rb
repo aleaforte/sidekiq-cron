fe73924b43eb0d56c32d49405c2cb7962d26af73
Nathan Appere <nathan.appere@gmail.com>
2018-06-22 09:25:25 -0700

    Add ENV option to disable SidekiqCron Launcher

--- lib/sidekiq/cron/launcher.rb
+++ lib/sidekiq/cron/launcher.rb
@@ -9,45 +9,50 @@ require 'sidekiq/cron/poller'
 #
 # we are creating new cron poller instance and
 # adding start and stop commands to launcher
-module Sidekiq
-  class Launcher
-    # Add cron poller to launcher
-    attr_reader :cron_poller
 
-    # remember old initialize
-    alias_method :old_initialize, :initialize
+if ENV['SIDEKIQ_CRON_ENABLED'].to_s.in?(['false', '0'])
 
-    # add cron poller and execute normal initialize of Sidekiq launcher
-    def initialize(options)
-      @cron_poller = Sidekiq::Cron::Poller.new
-      old_initialize options
-    end
+  module Sidekiq
+    class Launcher
+      # Add cron poller to launcher
+      attr_reader :cron_poller
 
-    # remember old run
-    alias_method :old_run, :run
+      # remember old initialize
+      alias_method :old_initialize, :initialize
 
-    # execute normal run of launcher and run cron poller
-    def run
-      old_run
-      cron_poller.start
-    end
+      # add cron poller and execute normal initialize of Sidekiq launcher
+      def initialize(options)
+        @cron_poller = Sidekiq::Cron::Poller.new
+        old_initialize options
+      end
 
-    # remember old quiet
-    alias_method :old_quiet, :quiet
+      # remember old run
+      alias_method :old_run, :run
 
-    # execute normal quiet of launcher and quiet cron poller
-    def quiet
-      cron_poller.terminate
-      old_quiet
-    end
+      # execute normal run of launcher and run cron poller
+      def run
+        old_run
+        cron_poller.start
+      end
 
-    # remember old stop
-    alias_method :old_stop, :stop
+      # remember old quiet
+      alias_method :old_quiet, :quiet
 
-    # execute normal stop of launcher and stop cron poller
-    def stop
-      cron_poller.terminate
-      old_stop
+      # execute normal quiet of launcher and quiet cron poller
+      def quiet
+        cron_poller.terminate
+        old_quiet
+      end
+
+      # remember old stop
+      alias_method :old_stop, :stop
+
+      # execute normal stop of launcher and stop cron poller
+      def stop
+        cron_poller.terminate
+        old_stop
+      end
     end
   end
+
 end
